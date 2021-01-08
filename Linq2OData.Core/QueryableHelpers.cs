cc8b21020c3437e0843dc383973883d1e4faecc6
Eduardo Vencovsky <eduvencovsky@gmail.com>
2021-01-08 12:20:18 -0300

    QueryableExtensions Improved

--- Linq2OData.Core/QueryableHelpers.cs
+++ Linq2OData.Core/QueryableHelpers.cs
@@ -5,8 +5,9 @@ using System.Linq.Expressions;
 
 namespace Linq2OData.Core
 {
-    public static class QueryableHelpers
+    public static class QueryableExtensions
     {
+        #region Public
         public static MethodCallExpression GetMethod<T>(this IQueryable<T> queryable, string method) 
         {
             var methodExpression = (MethodCallExpression)queryable.Expression;
@@ -30,35 +31,100 @@ namespace Linq2OData.Core
             return null;
         }
 
+        public static IEnumerable<MethodCallExpression> GetMethods<T>(this IQueryable<T> queryable, string method) 
+        {
+            var methodExpression = (MethodCallExpression)queryable.Expression;
+            var methods = new List<MethodCallExpression>();
+            while (methodExpression != null) {
+                if (methodExpression.Method.Name == method)
+                {
+                    methods.Add(methodExpression);
+                } 
+                else if (
+                    methodExpression.Arguments != null && 
+                    methodExpression.Arguments.Count > 1 && 
+                    methodExpression.Arguments?[0] is MethodCallExpression subMethodExpression
+                ){
+                    methodExpression = subMethodExpression;
+                }
+                else 
+                {
+                    return methods;
+                }
+            }
+            return methods;
+        }
+
         public static ReadOnlyCollection<Expression> GetMethodArguments<T>(this IQueryable<T> queryable, string method) 
         {
             return queryable.GetMethod(method)?.Arguments;
         }
 
+        public static IEnumerable<ReadOnlyCollection<Expression>> GetMethodsArguments<T>(this IQueryable<T> queryable, string method) 
+        {
+            var methods = queryable.GetMethods(method);
+            var methodsArguments = new List<ReadOnlyCollection<Expression>>();
+
+            if(methods != null && methods.Count() > 0)
+                return methodsArguments;
+
+            foreach(var m in methods){
+                methodsArguments.Add(m.Arguments);
+            }
+
+            return methodsArguments;
+        }
+
         public static IEnumerable<object> GetMethodArgumentsValues<T>(this IQueryable<T> queryable, string method)
         {
-            var arguments = queryable.GetMethodArguments(method)
-                .Skip(1); // skip first argument because it's the "this" parameter of the builder pattern
+            var arguments = queryable.GetMethodArguments(method);                
+            return GetMethodArgumentsValues(arguments);
+        }
+        
+        public static IEnumerable<IEnumerable<object>> GetMethodsArgumentsValues<T>(this IQueryable<T> queryable, string method)
+        {
+            var methodsArguments = queryable.GetMethodsArguments(method);                
+            var argumentsValues = new List<IEnumerable<object>>();
             
-            var resultArguments = new List<object>();   
-            
-            foreach(var arg in arguments)
-            {
-                if (arg is ConstantExpression constantExpression)
-                {
-                    resultArguments.Add(constantExpression.Value);
-                }
-                else 
-                {
+            foreach(var methodArguments in methodsArguments)
+            {              
+                argumentsValues.Add(GetMethodArgumentsValues(methodArguments));
+            }
 
-                }
-            } 
-            return resultArguments;
+            return argumentsValues;
         }
         
         public static bool HasMethod<T>(this IQueryable<T> queryable, string method)
         {
             return queryable.GetMethod(method) != null;        
         }
+        #endregion
+        
+        #region Private            
+        private static IEnumerable<object> GetMethodArgumentsValues(ReadOnlyCollection<Expression> arguments)
+        {
+            var resultArguments = new List<object>();
+
+            if(arguments == null)
+                return resultArguments;   
+            
+            foreach (var arg in arguments.Skip(1)) // skip first argument because it's the "this" parameter of the builder pattern
+            {
+                if (arg is ConstantExpression constantExpression)
+                {
+                    resultArguments.Add(constantExpression.Value);
+                }
+                else if (arg is UnaryExpression unaryExpression)
+                {
+                    resultArguments.Add(unaryExpression.Operand);
+                }
+                else 
+                {
+                    resultArguments.Add(arg);
+                }
+            } 
+            return resultArguments;
+        }
+        #endregion
     }
 }
--- Linq2OData.Tests/QueryableExtesionsTest.cs
+++ Linq2OData.Tests/QueryableExtesionsTest.cs
@@ -10,8 +10,9 @@ using Linq2OData.Core;
 namespace Linq2OData.Tests
 {
     [TestClass]
-    public class HasMethodTest
+    public class QueryableHelpersTest
     {
+        #region HasMethod
         [TestMethod]
         public void Test_HasWhereMethod()
         {
@@ -50,7 +51,7 @@ namespace Linq2OData.Tests
         }
 
         [TestMethod]
-        public void Test_SkipInsideWhereShouldReturnFalse()
+        public void Test_SkipInsideWhereShouldNotHaveMethod()
         {
             var query = new List<TestModel>().AsQueryable()
                 .Where(x => x.Collection.Skip(1).FirstOrDefault() == "hey");
@@ -61,7 +62,7 @@ namespace Linq2OData.Tests
         }
 
         [TestMethod]
-        public void Test_NextedQueryableSkipInsideWhereShouldReturnFalse()
+        public void Test_NextedQueryableSkipInsideWhereShouldNotHaveMethod()
         {            
             var query = new List<TestModel>().AsQueryable()
                 .Where(x => x.SelfCollection.AsQueryable()
@@ -72,7 +73,7 @@ namespace Linq2OData.Tests
         }
         
         [TestMethod]
-        public void Test_NextedQueryableTakeInsideWhereShouldReturnFalse()
+        public void Test_NextedQueryableTakeInsideWhereShouldNotHaveMethod()
         {
             var query = new List<TestModel>().AsQueryable()
                 .Where(x => x.SelfCollection.AsQueryable()
@@ -84,7 +85,7 @@ namespace Linq2OData.Tests
         }
         
         [TestMethod]
-        public void Test_MultipleWhereMethodsShouldReturnTrue()
+        public void Test_MultipleWhereMethodsShouldHaveMethod()
         {
             var query = new List<TestModel>().AsQueryable()
                 .Where(x => x.Id > 2)
@@ -96,7 +97,13 @@ namespace Linq2OData.Tests
             
             hasWhere.Should().BeTrue();
         }
+        #endregion
+        
+        #region GetMethod
+            
+        #endregion        
 
+        #region GetMethodArguments
         [TestMethod]
         public void Test_GetSkipArguments()
         {
@@ -131,8 +138,33 @@ namespace Linq2OData.Tests
             
             // this argument and numeric argument
             takeArguments.Should().HaveCount(2);            
-        }
+        }      
 
+        [TestMethod]
+        public void Test_MultipleWhereMethodsShouldAllArguments()
+        {            
+            var whereArguments = new List<Expression<Func<TestModel, bool>>> 
+            {
+                x => x.Id > 2,
+                x => x.IsSomething,
+                x => x.Title.Length > 0,
+                x => x.ExtraData.SomeNumber == 10
+            };
+
+            var query = new List<TestModel>().AsQueryable()
+                .Where(whereArguments[0])
+                .Where(whereArguments[1])
+                .Where(whereArguments[2])
+                .Where(whereArguments[3]);
+            
+            var resultArguments = query.GetMethodArguments(nameof(Queryable.Where));
+            
+            resultArguments.Should().NotBeNull();
+            resultArguments.Should().HaveCount(whereArguments.Count);
+        }  
+        #endregion
+
+        #region GetMethodArgumentsValues
         [TestMethod]
         public void Test_GetSkipArgumentValue()
         {
@@ -140,11 +172,88 @@ namespace Linq2OData.Tests
             var query = new List<TestModel>().AsQueryable()
                 .Skip(skipArgumentValue);
             
-            var takeArguments = query.GetMethodArgumentsValues(nameof(Queryable.Skip));
+            var skipArguments = query.GetMethodArgumentsValues(nameof(Queryable.Skip));
+                        
+            skipArguments.Should().NotBeNull();
+            skipArguments.Should().HaveCount(1); 
+            skipArguments.First().Should().Be(skipArgumentValue);           
+        }
+
+        [TestMethod]
+        public void Test_GetTakeArgumentValue()
+        {
+            var takeArgumentValue = 1;
+            var query = new List<TestModel>().AsQueryable()
+                .Take(takeArgumentValue);
+            
+            var takeArguments = query.GetMethodArgumentsValues(nameof(Queryable.Take));
                         
             takeArguments.Should().NotBeNull();
             takeArguments.Should().HaveCount(1); 
-            takeArguments.First().Should().Be(skipArgumentValue);           
+            takeArguments.First().Should().Be(takeArgumentValue);           
         }
+
+        [TestMethod]
+        public void Test_GetWhereArgumentValue()
+        {
+            Expression<Func<TestModel, bool>> whereArgumentValue = x => x.Id == 1;
+            var query = new List<TestModel>().AsQueryable()
+                .Where(whereArgumentValue);
+            
+            var whereArguments = query.GetMethodArgumentsValues(nameof(Queryable.Where));
+                        
+            whereArguments.Should().NotBeNull();
+            whereArguments.Should().HaveCount(1); 
+            whereArguments.First().Should().Be(whereArgumentValue);           
+        }
+
+        [TestMethod]
+        public void Test_GetNonExistingArgumentValue()
+        {            
+            var query = new List<TestModel>().AsQueryable()
+                .Skip(1);
+            
+            var whereArguments = query.GetMethodArgumentsValues("NON_EXISTING_METHOD");
+                        
+            whereArguments.Should().NotBeNull();
+            whereArguments.Should().HaveCount(0);             
+        }
+        
+        [TestMethod]
+        public void Test_MultipleWhereMethodsShouldAllArgumentsValues()
+        {             
+            var whereArguments = new List<Expression<Func<TestModel, bool>>> 
+            {
+                x => x.Id > 2,
+                x => x.IsSomething,
+                x => x.Title.Length > 0,
+                x => x.ExtraData.SomeNumber == 10
+            };
+
+            var query = new List<TestModel>().AsQueryable()
+                .Where(whereArguments[0])
+                .Where(whereArguments[1])
+                .Where(whereArguments[2])
+                .Where(whereArguments[3]);
+            
+            var resultArguments = query.GetMethodArgumentsValues(nameof(Queryable.Where));
+            
+            resultArguments.Should().NotBeNull();
+            resultArguments.Should().HaveCount(whereArguments.Count);
+            resultArguments.Should().BeEquivalentTo(whereArguments);
+        }
+        #endregion
+        
+        #region GetMethods
+            
+        #endregion
+        
+        #region GetMethodsArguments
+            
+        #endregion
+
+        #region GetMethodsArgumentsValues
+            
+        #endregion
     }
 }
